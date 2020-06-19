8d06c4a8fc5a06b096a78abb604c5936d7314183
Michael <michael.n.preston@gmail.com>
2020-06-18 22:30:47 -0500

    cleanup

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,13 +1,9 @@
-import { useContext } from 'react'
-import { EntryContext } from '../../contexts/EntryContext'
 import EntryList from '../../components/EntryList'
 import { useProjects } from '../../hooks/useProjects'
 
 const Projects = () => {
 
-    // window?.localStorage?.getItem('projects')
     let projects = useProjects();
-    // console.log('projects :>> ', projects);   
 
     console.info(`Loaded ${projects.length} project entries!: `)    
     let sortedEntries = projects.sort((a, b) => a.fields.order - b.fields.order) || []     
@@ -17,22 +13,7 @@ const Projects = () => {
             <h3><u>Projects</u></h3>
             <EntryList entries={sortedEntries} />
         </>
-    )
-    // return (<div>
-    //     {!!projects ?
-    //         <div>{projects.length}</div>
-    //         : <UnderConstruction message="No projects :'(" />}
-    // </div>)
-    // const context = useContext(EntryContext);
-    // // (
-    // // <EntryContext.Consumer>
-    // // {(context) => {
-
-
-    
-    // // }}
-    // // </EntryContext.Consumer>
-    // // )
+    )    
 }
 
 export default Projects
